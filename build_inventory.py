8d6cbd953162f28825d8ec2fdc00a9fd9aa476a5
d07056549-bit <d07056549@gmail.com>
2026-03-31 22:15:18 +0100

    Update build_inventory.py

--- build_inventory.py
+++ build_inventory.py
@@ -12,7 +12,7 @@ import pandas as pd
 # CONFIG
 # -------------------------------------------------------------------
 
-PROJECT_ROOT = Path(r"C:\Users\Empok\Documents\GitHub\Sofie\Data\raw")
+PROJECT_ROOT = Path(r"C:\Users\Empok\Documents\GitHub\Sofie")
 
 RAW_ROOT = PROJECT_ROOT / "Data" / "raw"
 PARQUET_CACHE = RAW_ROOT / "_parquet_cache"
